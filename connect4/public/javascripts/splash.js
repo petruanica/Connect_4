a8a0a695eb7ec0585384b54eba2a12b733149d09
Nicolae Filat <nicolae.filat@gmail.com>
2021-12-29 23:12:39 +0100

    Added form instead of javascript click event

--- connect4/public/javascripts/splash.js
+++
@@ -1,4 +0,0 @@
-const playButton = document.querySelector("#play");
-playButton.addEventListener("click", () => location = "./game");
-const tutorialButton = document.querySelector("#tutorial");
-tutorialButton.addEventListener("click", () => location = "./tutorial");
