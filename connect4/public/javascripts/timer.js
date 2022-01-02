5cf00561bfc2189e6e387dcc9ebd7b90839ee140
petruanica <petru.anica@gmail.com>
2022-01-02 21:03:37 +0200

    Added turn timer feature

--- connect4/public/javascripts/timer.js
+++ connect4/public/javascripts/timer.js
@@ -1,12 +1,19 @@
-const timer = document.querySelector('#clock>span');
+const clock = document.querySelector('#clock>span');
+const timer = document.querySelector('#timer>span');
 let seconds = 0;
+let secondsLeft = 15;
 
 function addOneSecond() {
     seconds++;
+    displayClock();
+}
+
+function substractOneSecond() {
+    secondsLeft--;
     displayTimer();
 }
 
-function displayTimer() {
+function displayClock() {
     let sec = seconds % 60;
     if (sec < 10) {
         sec = '0' + sec.toString();
@@ -14,6 +21,15 @@ function displayTimer() {
         sec = sec.toString();
     }
     const min = Math.floor(seconds / 60).toString();
-    timer.innerHTML = min + ':' + sec;
+    clock.innerHTML = min + ':' + sec;
 }
+
+function displayTimer() {
+    if (secondsLeft == -1) {
+        secondsLeft = 15;
+    }
+    timer.innerHTML = secondsLeft.toString();
+}
+
 setInterval(addOneSecond, 1000);
+setInterval(substractOneSecond, 1000);
